6e38c398af0cf43eca74044a7a8617aff8a37035
BeardAnnihilator <alexandre.bigmoustache@gmail.com>
2016-04-09 15:34:36 +1000

    add leaderboard

---
+++ CompeStrava/CompeStrava/Controllers/LeaderBoardController.cs
@@ -0,0 +1,19 @@
+﻿using CompeStrava.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CompeStrava.Controllers
+{
+    public class LeaderBoardController : Controller
+    {
+        //
+        // GET: /LeaderBoard/
+        public ActionResult Index()
+        {
+            return View(new LeaderBoardViewModel());
+        }
+	}
+}
---
+++ CompeStrava/CompeStrava/ViewModel/LeaderBoardViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CompeStrava.ViewModel
+{
+    public class LeaderBoardViewModel
+    {
+    }
+}
