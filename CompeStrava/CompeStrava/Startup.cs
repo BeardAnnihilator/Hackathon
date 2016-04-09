c9f1d2ddaf501d2af50f0ce4a95d31cb4b691c42
BeardAnnihilator <alexandre.bigmoustache@gmail.com>
2016-04-09 12:06:55 +1000

    initial commit

---
+++ CompeStrava/CompeStrava/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CompeStrava.Startup))]
+namespace CompeStrava
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
